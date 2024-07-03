b63c054d7ebf69b1959973097164bb6af19f265d
emoeller <emoeller@ur.rochester.edu>
2024-07-03 13:29:59 -0400

    Working on getting a test GNN model working. Currently, it seems like the cell names are not being associated to the correct starting states

--- scBONITA/expression_histogram.py
+++ scBONITA/expression_histogram.py
@@ -1,80 +1,74 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-datafile = "../../george_data/hiv_dataset/HIV_dataset_normalized_integrated_counts.csv"
+def plot_single_histogram(data):
+    array = data.values.flatten()
 
-# "../../kazer_data/merged_data.csv"
+    filtered_array = array[array != 0]
 
-data = pd.read_csv(datafile, index_col=0)
+    # Set up the figure
+    plt.figure(figsize=(5, 6))
 
-array = data.values.flatten()
+    plt.hist(filtered_array, bins=250, log=False)
+    plt.title('Histogram of scRNAseq Gene Expression')
+    plt.xlabel('log2 Normalized Counts')
+    plt.ylabel('log10 Frequency')
+    plt.xlim((-0.5, 2.5))
+    plt.tight_layout()
+    plt.show()
 
-filtered_array = array[array != 0]
+def plot_individual_gene_histograms(data):
+    # Extract all expression values from the dataset
+    expression_values = data.iloc[:, 1:51].values
 
-# Create a figure and a set of subplots
-fig, axs = plt.subplots(1, 2, figsize=(12, 6))  # 2 rows, 1 column of subplots
+    # Set up the figure
+    plt.figure(figsize=(10, 6))
 
-# Plotting the histogram of the filtered array (non-zero values) with log scale
-axs[0].hist(filtered_array, bins=250, log=False)
-axs[0].set_title('Frequency of Expression Values')
-axs[0].set_xlabel('Expression Value')
-axs[0].set_ylabel('Frequency')
-axs[0].set_xlim((-0.5, 5))
+    # Plot the distribution of each gene with low opacity
+    for i in range(expression_values.shape[1]):
+        gene_expression_values = expression_values[:, i]
+        non_zero_gene_expression_values = gene_expression_values[gene_expression_values > 0]
+        plt.hist(non_zero_gene_expression_values, bins=100, alpha=0.1, edgecolor='none', log=False)
+
+    plt.title('Distribution of Expression Values')
+    plt.xlabel('Expression Value')
+    plt.ylabel('Frequency')
+    plt.xlim((-0.5, 5))
+    plt.ylim((0, 5000))
+    plt.grid(True)
+    plt.show()
+
+def plot_total_gene_expression_histogram(data):
+    array = data.values.flatten()
+
+    filtered_array = array[array != 0]
+
+    # Create a figure and a set of subplots
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))  # 2 rows, 1 column of subplots
+
+    # Plotting the histogram of the filtered array (non-zero values) with log scale
+    axs[0].hist(filtered_array, bins=250, log=False)
+    axs[0].set_title('Frequency of Expression Values')
+    axs[0].set_xlabel('Expression Value')
+    axs[0].set_ylabel('Frequency')
+    axs[0].set_xlim((-0.5, 5))
 
 
-# Plotting the histogram of all values (including zeros) with log scale
-axs[1].hist(filtered_array, bins=250, log=False)
-axs[1].set_title('Zoomed in View of Expression Frequencies')
-axs[1].set_xlabel('Expression Value')
-axs[1].set_ylabel('Frequency')
-axs[1].set_xlim((-0.5, 5))
-axs[1].set_ylim((0, 0.30e6))
+    #Plotting the histogram of all values (including zeros) with log scale
+    axs[1].hist(filtered_array, bins=250, log=False)
+    axs[1].set_title('Zoomed in View of Expression Frequencies')
+    axs[1].set_xlabel('Expression Value')
+    axs[1].set_ylabel('Frequency')
+    axs[1].set_xlim((-0.5, 5))
+    axs[1].set_ylim((0, 0.30e6))
 
-# Show the plots
-plt.tight_layout()  # Adjusts subplot params so that subplots fit into the figure area.
-plt.show()
+    #Show the plots
+    plt.tight_layout()  # Adjusts subplot params so that subplots fit into the figure area.
+    plt.show()
 
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import math
-
-# # Data Setup
-# pathways = ['Apoptosis', 'Cell adhesion\nmolecules (CAMs)', 'Complement and \ncoagulation cascades', 'Glycolysis / Gluconeogenesis']
-# data = {
-#     'BON': [1.33, 0.10, 2.56, 0.05],
-#     'CLP': [0.64, 0.64, 0.39, 0.51],
-#     'CAM': [0.42, 1.46, 0.01, 1.44]
-# }
-
-# p_value = 0.05
-# negative_log10_p_value = -math.log10(p_value)
-# negative_log10_p_value
-
-# # X locations for the groups
-# ind = np.arange(len(pathways))
-# width = 0.25  # width of the bars
-
-# fig, ax = plt.subplots()
-
-# # Creating bars
-# bars1 = ax.bar(ind - width, data['BON'], width, label='BONITA')
-# bars2 = ax.bar(ind, data['CLP'], width, label='CLIPPER')
-# bars3 = ax.bar(ind + width, data['CAM'], width, label='CAMERA')
-
-# # Adding labels, title, and custom x-axis tick labels
-# ax.set_ylabel('-log10 p-values')
-# ax.set_title('Differentially regulated pathways between infants with mild and severe RSV infection')
-# ax.set_xticks(ind)
-# ax.set_xticklabels(pathways)
-# ax.legend()
-
-# # Adding a horizontal dashed line at y=1.30
-# ax.axhline(y=negative_log10_p_value, color='r', linestyle='--')
-# ax.legend()
-
-# # Adding some text for labels, title, and custom x-axis tick labels, etc.
-# plt.tight_layout()
-
-# plt.show()
+if __name__ == '__main__':
+    datafile = "../../george_data/hiv_dataset/HIV_dataset_normalized_integrated_counts.csv"
+    data = pd.read_csv(datafile, index_col=0)
+    plot_single_histogram(data)
 
 
